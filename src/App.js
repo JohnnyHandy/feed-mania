57286c2cc6d0b9f8593407c5421c00a349b8ac04
EC2 Default User <ec2-user@ip-172-31-44-207.ec2.internal>
2019-09-13 19:18:55 +0000

    added more stringkeys

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const RSSParser = require('rss-parser');
 class App extends Component {
  getWiredFeed(){
    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-    axios.get(CORS_PROXY+'http://voxeu.org/feed/popular/rss.xml')
+    axios.get(CORS_PROXY+'http://www.slate.com/articles/news_and_politics/politics.teaser.all.10.rss/')
     .then((res) => {
       let parser = new RSSParser();
       parser.parseString(res.data, (err, feed)=> {
--- src/utility/stringKeys.js
+++ src/utility/stringKeys.js
@@ -1,10 +1,51 @@
-export const BBC_NEWS = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml'; //RSS-PARSER http://feeds.bbci.co.uk/news/world/rss.xml
-export const ESPN = 'https://feed2json.org/convert?url=http%3A%2F%2Fwww.espn.com%2Fespn%2Frss%2Fnews'; //RSS-PARSER
+//WORLD
+export const NY_POST='https://nypost.com/feed/' //RSS-FEED (TITLE AND CONTENTSNIPPET)
+export const BBC_NEWS = 'http://feeds.bbci.co.uk/news/world/rss.xml'; //RSS-PARSER (TITLE AND CONTENTSNIPPET)
 export const FOX_NEWS = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.foxnews.com%2Ffoxnews%2Fworld'; //XMLHTTP
 export const NY_TIMES = 'https://feed2json.org/convert?url=https%3A%2F%2Frss.nytimes.com%2Fservices%2Fxml%2Frss%2Fnyt%2FWorld.xml'; //RSS-PARSER https://rss.nytimes.com/services/xml/rss/nyt/World.xml
 export const REUTERS = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.reuters.com%2FReuters%2FworldNews'; //XMLHTTP (TITLE AND SUMMARY)
 export const WALL_STREET = 'https://feed2json.org/convert?url=https%3A%2F%2Ffeeds.a.dj.com%2Frss%2FRSSWorldNews.xml'; // RSS - PARSER https://feeds.a.dj.com/rss/RSSWorldNews.xml
-export const VOX = 'https://feed2json.org/convert?url=http%3A%2F%2Fwww.vox.com%2Frss%2Findex.xml';  //REMOVE VOX
-export const CNN = 'https://feed2json.org/convert?url=http%3A%2F%2Frss.cnn.com%2Frss%2Fcnn_world.rss';
-export const ABC = 'https://feed2json.org/convert?url=http%3A%2F%2Fabcnews.go.com%2Fabcnews%2Ftopstories';
-export const YAHOO_NEWS = 'https://feed2json.org/convert?url=https%3A%2F%2Fwww.yahoo.com%2Fnews%2Frss%2Ftopstories'
+export const VOX = 'https://feed2json.org/convert?url=https%3A%2F%2Fwww.huffpost.com%2Fsection%2Ffront-page%2Ffeed';  //XMLHTTP
+export const CNN = 'https://feed2json.org/convert?url=http%3A%2F%2Frss.cnn.com%2Frss%2Fcnn_world.rss'; //RSS-PARSER
+export const ABC = 'https://feed2json.org/convert?url=http%3A%2F%2Fabcnews.go.com%2Fabcnews%2Ftopstories'; //RSS-PARSER
+export const YAHOO_NEWS = 'https://feed2json.org/convert?url=https%3A%2F%2Fwww.yahoo.com%2Fnews%2Frss%2Ftopstories' //RSS-PARSER
+
+
+//SPORTS
+export const ESPN = 'https://feed2json.org/convert?url=http%3A%2F%2Fwww.espn.com%2Fespn%2Frss%2Fnews'; //RSS-PARSER
+export const BBC_SPORTS='http://feeds.bbci.co.uk/sport/rss.xml?edition=int#' //RSS-FEED (TITLE AND CONTENTSNIPPET)
+export const NFL = 'https://feed2json.org/convert?url=http%3A%2F%2Fwww.nfl.com%2Frss%2Frsslanding%3FsearchString%3Dhome' //XMLHTTP (title and summay)
+export const FOX_SPORTS='https://feed2json.org/convert?url=https%3A%2F%2Fapi.foxsports.com%2Fv1%2Frss%3FpartnerKey%3DzBaFxRyGKCfxBagJG9b8pqLyndmvo7UU' //XMLHTTP (title and summary should be different)
+export const FIFA = 'https://www.fifa.com/rss-feeds/news' //RSS-PARSER(TITHLE, IMAGE AND CONTENT:ENCODED)
+export const CBS_SPORTS='http://www.cbssports.com/rss/headlines' //RSS-PARSER(TITLE, CONTENT OR SNIPPET)
+
+
+//TECHNOLOGY
+export const NY_TIMES_TECH ='http://feeds.nytimes.com/nyt/rss/Technology' //RSS-FEED (TITLE AND CONTENTSNIPPET)
+export const WIRED = 'http://feeds.wired.com/wired/index' //RSS-FEED (TITLE AND SNIPPET)
+export const PC_WORLD = 'http://feeds.pcworld.com/pcworld/latestnews' //RSS-FEED(TITLE, SNIPPET AND IMAGE)
+export const TECH_WORLD = 'http://www.techworld.com/news/rss' //RSS-FEED (TITLE AND SNIPPET)
+export const LIFE_HACKER = 'https://lifehacker.com/rss'//RSS-FEED(TITLE, SNIPPET AND CONTENT IMG TAG)
+export const READWRITE='http://readwrite.com/feed/' //RSS-FEED (TITLE, SNIPPET AND CONTENT ENCODED WITH IMAGE)
+export const GIZMODO = 'https://gizmodo.com/rss' //RSS-FEED (TITLE, SNIPPET AND CONTENT IMG TAG)
+export const TECHNOLOGY_REVIEW='https://www.technologyreview.com/topnews.rss' //RSS-FEED (TITLE AND SNIPPET)
+export const HOW_TO_GEEK = 'https://www.lifesavvy.com/p/uploads/2019/09/563db375.jpg'//RSS-FEED(TITLE AND CONTENT:ENCODED with image)
+
+//BUSSINESS
+export const THE_ATLANTIC_BUSINESS = 'https://www.theatlantic.com/feed/channel/business/' //RSS-FEED(TITLE AND SNIPPET)
+export const ENTREPENEUR = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.feedburner.com%2Fentrepreneur%2Flatest' //XMLHTTP (TITLE AND SUMMARY NEEDING TAG FIX)
+export const HARVARD_BUSINESS='https://news.harvard.edu/gazette/section/business-economy/feed/' //RSS-FEED (TITLE, SNIPPET, ENCLOSURE WITH IMAGE TAG, CONTENT:ENCODED) 
+export const FREAKONOMICS ='http://freakonomics.com//feed/' //RSS-FEED TITLE AND CONTENT SNIPPET)
+export const THE_BIG_PICTURE ='http://feeds.feedburner.com/TheBigPicture' //RSS-FEED (TITLE AND SNIPPET)
+export const THE_ECONOMIST_BUSINESS = 'https://www.economist.com/business/rss.xml'//RS
+export const HUFFINGTON_BUSINESS ='https://feed2json.org/convert?url=https%3A%2F%2Fwww.huffingtonpost.com%2Fsection%2Fbusiness%2Ffeed' //XMLHTTP(TITLE AND SUMMARY)
+export const FORTUNE = 'http://fortune.com/feed/' //RSS-FEED (title and snippet)
+
+//POLITICS
+export const FOX_NEWS_POLITICS = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.foxnews.com%2Ffoxnews%2Fpolitics' //XMLTHHP(TITLE AND SUMMARY NEEDING TAG FIX)
+export const CNN_POLITICS = 'http://rss.cnn.com/rss/cnn_allpolitics.rss' //RSS-PARSER (TITLE AND SNIPPET)
+export const REUTERS_POLITICS = 'https://feed2json.org/convert?url=http%3A%2F%2Ffeeds.reuters.com%2FReuters%2FPoliticsNews' //XMLHTTP(TITLE AND SUMMARY NEED TAG FIX)
+export const SLATE = 'http://www.slate.com/articles/news_and_politics/politics.teaser.all.10.rss/' //RSS-PARSER (TITLE AND SNIPPET)
+//GAMING
+
+//HEALTH
