55d7c2a7dd70bc1083bd8053530c686335ed2f92
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-11-19 18:38:44 -0300

    refactoring layout

--- src/UI/Layout/layout.js
+++ src/UI/Layout/layout.js
@@ -7,12 +7,24 @@ import './layout.css'
 
 const layout = (props) => {
     return(
-        <div>
-            <Navbar/>
+        <div className='container'>
+            <div className='header-title'> 
+                <div>FEED </div><div>MANIA</div>
+            </div>
+            <div className='main-navbar'>
+                <div className='main-item'>World</div>
+                <div className='main-item'>Sports</div>
+                <div className='main-item'>Technology</div>
+                <div className='main-item'>Business</div>
+                <div className='main-item'>Politics</div>
+                <div className='main-item'>Gaming</div>
+                <div className='main-item'>Health</div>
+            </div>
+            {/* <Navbar/>
             <div className='layout'>
             <SideBar changeFeed = {props.changeFeed}/>
             <Content contentData = {props.contentData}/>
-            </div>
+            </div> */}
         </div>
     )
 }
